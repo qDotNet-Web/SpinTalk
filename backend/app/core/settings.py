82397a50748b4a5dc8617b4637e76e85c142a860
Marvin Brückner <marvin@qdotnet.eu>
2024-05-29 19:08:33 +0200

    First implementation for register and login routes

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -1,13 +1,20 @@
-from pydantic_settings import BaseSettings
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
 import os
 
 
-class Settings(BaseSettings):
+class Settings():
     MONGODB_URL = os.getenv("MONGODB_URL")
     MONGODB_DB_ACCOUNTS = "accounts_db"
     MONGODB_DB_REPORTS = "reports_db"
     MONGODB_DB_CHATS = "chats_db"
     SECRET_KEY = os.getenv("SECRET_KEY")
+
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+    pwd_context = CryptContext(schemas=["bcrypt"], deprecated="auto")
+    oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 
 settings = Settings()
