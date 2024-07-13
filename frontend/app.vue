f601c54fc5608685c9d86617177b3e8b5cc3aff5
knauerleo <bloqoel@gmail.com>
2024-07-13 20:32:25 +0200

    chore: Update WebSocket connection URL in websockets.ts

--- frontend/app.vue
+++ frontend/app.vue
@@ -13,4 +13,5 @@ import 'primevue/resources/themes/aura-dark-green/theme.css'
 import '~/assets/css/primevue.css'
 import '~/assets/css/style.css'
 import 'primeicons/primeicons.css'
+import {connect, disconnect, send} from '~/communication/websockets.ts'
 </script>
--- frontend/communication/websockets.ts
+++ frontend/communication/websockets.ts
@@ -2,6 +2,16 @@ let ws: WebSocket;
 
 export const connect = (url: string) => {
   ws = new WebSocket(url);
+  console.log('Connecting to', url);
+  ws.onopen = function(event) {
+    console.log('Connection is open');
+  };
+  ws.onerror = function(error) {
+    console.error(`WebSocket Error: ${JSON.stringify(error)}`);
+  };
+  ws.onclose = function(event) {
+    console.log(`WebSocket closed: ${event.code} ${event.reason}`);
+  };
     return ws;
 };
 
@@ -13,4 +23,5 @@ export const send = (message: string) => {
   ws.send(message);
 };
 
-connect('ws://localhost:8000/ws');
+
+connect('ws://localhost:3000/ws');
